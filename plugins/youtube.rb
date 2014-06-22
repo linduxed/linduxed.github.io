076723259d195516bc74ce3bc2dd151e0af3b81a
Mikael Muszynski <linduxed@gmail.com>
2014-06-22 16:21:21 +0200

    Modify Youtube plugin to take width and height

--- plugins/youtube.rb
+++ plugins/youtube.rb
@@ -1,12 +1,23 @@
+# Syntax {% youtube video_id [width height] %}
+# Default width and height provided that works well with page layout.
+#
+# Examples:
+# {% youtube a1b2c3d4f5g %}
+# {% youtube a1b2c3d4f5g 720 480 %}
+
 module Jekyll
   class Youtube < Liquid::Tag
-    def initialize(name, id, tokens)
+    def initialize(name, markup, tokens)
+      if markup =~ /(\S+{11})( (\d+) (\d+))?/
+        @id = $1
+        @width = $3 || 605
+        @height = $4 || 360
+      end
       super
-      @id = id
     end
 
     def render(context)
-      %(<div class="embed-video-container"><iframe src="http://www.youtube.com/embed/#{@id.strip}" allowfullscreen></iframe></div>)
+      %(<div class="embed-video-container"><iframe src="http://www.youtube.com/embed/#{@id}" width="#{@width}" height="#{@height}" frameborder="0" allowfullscreen></iframe></div>)
     end
   end
 end
